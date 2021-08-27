ee529f32ee2b15bdac28be35758a3415bf5c961c
Chris Clark <chris.clark@globalstripe.com>
2021-08-27 17:34:23 +0100

    work in progress

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from "@aws-cdk/aws-lambda";
+
+// cdk bootstrap aws://494176534905/eu-west-2
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'VerizonVODIngest', {
+      versioned: true
+    });
+
+    const handler = new lambda.Function(this, "ingestor", {
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+      code: lambda.Code.fromAsset("resources"),
+      handler: "widgets.main",
+      environment: {
+        BUCKET: bucket.bucketName
+      }
+    });
+
+    bucket.grantReadWrite(handler); // was: handler.role);
+      
+
   }
 }
+
